4ebf91b4b84320a5441f54dad7e2330f60205db5
wolffbruno <wolffbruno72@gmail.com>
2023-11-22 17:37:51 -0300

    feat: add Terraform configuration files for Azure deployment
    - Added `.github/workflows/terraform.yml` file to define a GitHub Actions workflow for running Azure Login with OpenID Connect on push to the main branch.
    - Added `provider.tf` file to define the Azure Terraform provider and create a blob storage account resource.

---
+++ provider.tf
@@ -0,0 +1,21 @@
+// AZURE TERRAFORM PROVIDER
+provider "azurerm" {
+  features {}
+}
+
+# Path: main.tf
+# create a blob storage account
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "storageaccountterraform"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+}
